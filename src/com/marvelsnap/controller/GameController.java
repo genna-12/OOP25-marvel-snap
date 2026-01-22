a27a8d85f7ff3222df9fd6048ca7efaf6b615e35
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2026-01-22 19:24:28 +0100

    Aggiunge struttura di base a classe GameController

--- src/com/marvelsnap/controller/GameController.java
+++ src/com/marvelsnap/controller/GameController.java
@@ -1,5 +1,26 @@
 package com.marvelsnap.controller;
 
+import com.marvelsnap.model.*;
+import com.marvelsnap.view.*;
 public class GameController {
-    
+    private Game game;
+    private GamePanel view;
+    private InputState inputState;
+    private Card selectedCard;
+
+    public void onCardClicked(final Card card) {
+        return;
+    }
+
+    public void onLocationClicked(final int locIdx) {
+        return;
+    }
+
+    public void onEndTurnClicked() {
+        return;
+    }
+
+    public void onIntermissionReadyClicked() {
+        return;
+    }
 }
