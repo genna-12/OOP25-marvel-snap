4465ec6adee62b0ae9fcf96af2cebfde7edfbaa8
Marco Lumini <marco.lumini2@studio.unibo.it>
2026-01-22 15:31:15 +0100

    aggiunge signatures con return di valori dummy

--- src/com/marvelsnap/model/Deck.java
+++ src/com/marvelsnap/model/Deck.java
@@ -1,5 +1,15 @@
 package com.marvelsnap.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Deck {
-    
+    private List<Card> cards = new ArrayList<>();
+
+    public void shuffle(){}
+
+    public Card draw(){
+        return null;
+    }
+
 }
--- src/com/marvelsnap/model/Player.java
+++ src/com/marvelsnap/model/Player.java
@@ -1,5 +1,17 @@
 package com.marvelsnap.model;
 
 public class Player {
-    
+    private String name;
+    private int currentEnergy;
+    private int totalScore;
+
+    public void drawCard(){}
+
+    public void playCard(){}
+
+    public void resetEnergy(int amount){}
+
+    public Hand getHand(){
+        return null;
+    }
 }
