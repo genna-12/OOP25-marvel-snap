086f72fa816b48f2333822390044e44c47827d07
Marco Lumini <marco.lumini2@studio.unibo.it>
2026-02-02 15:59:57 +0100

    aggiunge implementazione metodi della classe CardPanel

--- src/main/java/com/marvelsnap/view/CardPanel.java
+++ src/main/java/com/marvelsnap/view/CardPanel.java
@@ -1,14 +1,52 @@
 package com.marvelsnap.view;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import com.marvelsnap.model.Card;
 
 public class CardPanel extends JPanel {
     private Card card;
-    private boolean isSelected;
+    private boolean isSelected = false;
 
-    public void setCard(Card c){}
+    public CardPanel(){
+        this.setSize(100, 200);
+        this.setBackground(Color.WHITE);
+        this.setLayout(new BorderLayout());
 
-    public void toggleSelection(){}
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e){
+                toggleSelection();
+            }
+        });
+    }
+
+    public void setCard(Card c){
+        this.card = c;
+        JLabel cardNameLabel = new JLabel();
+        cardNameLabel.setText(card.getName());  
+        JLabel cardCostLabel = new JLabel();
+        cardNameLabel.setText("COST = " + card.getCost());  
+        JLabel cardPowerLabel = new JLabel();
+        cardNameLabel.setText("POWER = " + card.getPower());  
+        this.add(cardNameLabel, BorderLayout.CENTER);
+        this.add(cardCostLabel, BorderLayout.SOUTH);
+        this.add(cardPowerLabel, BorderLayout.NORTH);
+    }
+
+    public void toggleSelection(){
+        isSelected = !isSelected;
+
+        if(isSelected == false){
+            this.setBackground(Color.GRAY);
+        }
+        else{
+            this.setBackground(Color.WHITE);
+        }
+        
+    }
 }
