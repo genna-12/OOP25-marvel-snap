c46f3aa8face4bffccca08469e8a516085fe1ac5
Marco Lumini <marco.lumini2@studio.unibo.it>
2026-02-06 11:50:50 +0100

    aggiunge costruttore nella classe HandPanel, piccole modifiche grafiche a CardPanel

--- src/main/java/com/marvelsnap/view/CardPanel.java
+++ src/main/java/com/marvelsnap/view/CardPanel.java
@@ -1,5 +1,6 @@
 package com.marvelsnap.view;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -16,10 +17,10 @@ public class CardPanel extends JPanel {
     private boolean isSelected = false;
 
     public CardPanel(){
-        this.setPreferredSize(new Dimension(200, 200));
-        this.setBackground(Color.BLACK);
+        this.setPreferredSize(new Dimension(150, 190));
+        this.setBackground(new Color(210, 210, 210));
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
         this.setLayout(new BorderLayout());
-        
 
         this.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e){
@@ -32,16 +33,22 @@ public class CardPanel extends JPanel {
         this.card = c;
         JLabel cardNameLabel = new JLabel();
         cardNameLabel.setText(card.getName()); 
-        cardNameLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        cardNameLabel.setForeground(Color.BLACK);
+        cardNameLabel.setFont(new Font("Arial", Font.BOLD, 18));
         cardNameLabel.setHorizontalAlignment(JLabel.CENTER); 
+
         JLabel cardCostLabel = new JLabel();
-        cardCostLabel.setText("COST = " + card.getCost());  
+        cardCostLabel.setText("COST = " + card.getCost()); 
+        cardCostLabel.setForeground(Color.BLACK); 
         cardCostLabel.setHorizontalAlignment(JLabel.CENTER);
         cardCostLabel.setPreferredSize(new Dimension(100, 40));
+
         JLabel cardPowerLabel = new JLabel();
         cardPowerLabel.setText("POWER = " + card.getPower()); 
+        cardPowerLabel.setForeground(Color.BLACK);   
         cardPowerLabel.setHorizontalAlignment(JLabel.CENTER); 
         cardPowerLabel.setPreferredSize(new Dimension(100, 40));
+        
         this.add(cardNameLabel, BorderLayout.CENTER);
         this.add(cardCostLabel, BorderLayout.SOUTH);
         this.add(cardPowerLabel, BorderLayout.NORTH);
@@ -49,14 +56,12 @@ public class CardPanel extends JPanel {
 
     public void toggleSelection(){
         isSelected = !isSelected;
-
         if(isSelected == true){
-            this.setBackground(Color.DARK_GRAY);
+            this.setBackground(Color.GRAY);
         }
         else{
-            this.setBackground(Color.BLACK);
+            this.setBackground(new Color(210, 210, 210));
         }
-
         repaint();
         
     }
